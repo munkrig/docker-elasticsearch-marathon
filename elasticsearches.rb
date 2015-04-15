9698433fc54c7942953234f9a186853d36d7919c
Tobi <tobilg@users.noreply.github.com>
2015-04-15 14:28:38 +0200

    Update elasticsearches.rb

--- elasticsearches.rb
+++ elasticsearches.rb
@@ -5,6 +5,9 @@ require 'json'
 marathon_url = ARGV[0]
 app_id = ARGV[1]
 
+print marathon_url
+print app_id
+
 tasks_url = "#{marathon_url}/v2/apps/#{app_id}/tasks"
 json = `curl -L #{tasks_url} 2>/dev/null`
 data = JSON.parse(json)
