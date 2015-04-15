611cb6f99dbb15aa7c73f9fed2aa4a2d2736d08f
Tobi <tobilg@users.noreply.github.com>
2015-04-15 15:09:04 +0200

    Fixed http call and parsing

--- elasticsearches.rb
+++ elasticsearches.rb
@@ -1,14 +1,22 @@
 #!/usr/bin/env ruby
 
+require 'net/http'
 require 'json'
+require 'uri'
 
 marathon_url = ARGV[0]
 app_id = ARGV[1]
 
-print marathon_url
-print app_id
-
 tasks_url = "#{marathon_url}/v2/apps/#{app_id}/tasks"
-json = `curl -L #{tasks_url} 2>/dev/null`
-data = JSON.parse(json)
+
+uri = URI.parse(tasks_url)
+http = Net::HTTP.new(uri.host, uri.port)
+
+request = Net::HTTP::Get.new(uri.path)
+request['Content-Type'] = 'application/json'
+request['Accept'] = 'application/json'
+
+response = http.request(request)
+
+data = JSON.parse(response.body)
 print data['tasks'].map { |t| "#{t['host']}:#{t['ports'].first}" }.join(',')
