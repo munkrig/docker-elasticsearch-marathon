6668c3db186e5e620f3f3d0614a7888c11e70b4e
munkrig <munkrig@users.noreply.github.com>
2015-06-11 10:27:20 +0200

    Update elasticsearch-marathon-bootstrap.sh

--- elasticsearch-marathon-bootstrap.sh
+++ elasticsearch-marathon-bootstrap.sh
@@ -4,16 +4,4 @@ hosts=$(/usr/local/bin/elasticsearches.js $MARATHON_URL $APP_ID)
 node_name="${APP_ID}-${PORT0}"
 cluster_name="${ELASTICSEARCH_CLUSTER_NAME}"
 
-exec /elasticsearch/bin/elasticsearch \
---node.name=${node_name} \
---cluster.name=${cluster_name} \
---network.publish_host=${HOST} \
---discovery.zen.ping.multicast.enabled=false \
---discovery.zen.ping.unicast.hosts=${hosts} \
---discovery.zen.ping_timeout=30s \
---discovery.zen.join_timeout=300s \
---discovery.zen.publish_timeout=300s \
---http.port=9200 \
---transport.tcp.port=9300 \
---transport.publish_port=${PORT1} \ 
- ${ES_OPTIONS}
+eval "/elasticsearch/bin/elasticsearch --node.name=${node_name} --cluster.name=${cluster_name} --network.publish_host=${HOST} --discovery.zen.ping.multicast.enabled=false --discovery.zen.ping.unicast.hosts=${hosts} --discovery.zen.ping_timeout=30s --discovery.zen.join_timeout=300s --discovery.zen.publish_timeout=300s --http.port=9200 --transport.tcp.port=9300 --transport.publish_port=${PORT1} ${ES_OPTIONS}"
